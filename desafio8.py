5b36bdaf5580c9b44795330ff7070aa9c615d6ba
Ivan Oliveira <126297390+Ivanolivei@users.noreply.github.com>
2023-06-01 18:12:25 -0300

    Add files via upload

---
+++ desafio8.py
@@ -0,0 +1,2 @@
+x = float(input('Digite a medida em Metros '))
+print(f'A medida {x}m vale em kilometro {x/1000}km\nA medida {x}m vale em hectometro {x/100}hm\nA medida {x}m vale em decâmetro {x/10}dam\nA medida {x}m vale em decimetro {x*10}dm \nA medida {x}m vale  em centimetros {x*100}cm\nA medida {x}m vale em milimetros {x*1000}mm')
