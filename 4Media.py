c07d0c85690b2c4bcae55f740d03b3a9f268da6d
Ivan Oliveira <126297390+Ivanolivei@users.noreply.github.com>
2023-06-01 18:11:43 -0300

    Update and rename desafio7.py to 4Media.py

---
+++ 4Media.py
@@ -0,0 +1,6 @@
+x = float(input('Qual foi sua nota no primeiro bimestre ? '))
+y = float(input('Qual foi a sua nota no segundo bimestre ? '))
+z = float(input('Qual foi a sua nota no segundo bimestre ? '))
+a = float(input('Qual foi a sua nota no segundo bimestre ? '))
+print(f'A sua media anual é {(x+y+z+a)/4:.1f}')
+
--- desafio7.py
+++
@@ -1,4 +0,0 @@
-x = float(input('Qual foi sua nota no primeiro bimestre ? '))
-y = float(input('Qual foi a sua nota no segundo bimestre ? '))
-print(f'A sua nota semestral é {(x+y)/2:.1f}')
-
